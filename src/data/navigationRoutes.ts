4dc7bf089fc71b952cf8515b34dba4b136920c74
Vanraj7 <jhalavanraj777@gmail.com>
2025-11-03 13:12:33 +0530

    add resume

--- src/data/navigationRoutes.ts
+++ src/data/navigationRoutes.ts
@@ -4,4 +4,5 @@ export const routes: NavbarRoutes = [
   { title: "Home", href: "/" },
   { title: "About", href: "/about" },
   { title: "Projects", href: "/projects" },
+  { title: "Resume", href: "https://drive.google.com/file/d/1OCN_6a980DKONQtpP69sKSChkww1Quec/view?usp=sharing" },
 ];
